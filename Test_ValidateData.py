ddb939aa58ae8d9350039342aeaef097d7bdca90
parthikdavra <62086803+parthikdavra@users.noreply.github.com>
2021-10-31 15:03:05 -0400

    added US15-US16

--- Test_ValidateData.py
+++ Test_ValidateData.py
@@ -2,7 +2,8 @@ import unittest
 
 from Family import Family
 from Individual import Individual
-from ValidateData import us01_dates_before_current_date, us02_birth_before_marriage, us07_age_less_than_150, us08_birth_before_marriage_of_parents
+
+from ValidateData import us01_dates_before_current_date, us02_birth_before_marriage, us03_birth_before_death,us04_marriage_before_divorce,us07_age_less_than_150, us08_birth_before_marriage_of_parents,us05_marriage_before_death, us06_divorce_before_death, us15_fewer_than_15_siblings, us16_male_last_names
 
 
 class TestValidateDataMethod(unittest.TestCase):
@@ -43,24 +44,87 @@ class TestValidateDataMethod(unittest.TestCase):
 
         self.individual.set_birth_date("12 DEC 2068")
         self.assertTrue(us02_birth_before_marriage(self.individual.birth_date, self.family.marriage_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+    
+    def test_us05_marriage_before_death(self):
+        self.assertTrue(us05_marriage_before_death(self.family.marriage_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+
+        self.family.set_marriage_date("05 Jan 1999")
+        self.individual.set_death_date("21 Mar 1860")
+        self.assertFalse(us05_marriage_before_death(self.family.marriage_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+
+        self.family.set_marriage_date("05 Jan 1992")
+        self.individual.set_death_date("21 Mar 2060")
+        self.assertTrue(us05_marriage_before_death(self.family.marriage_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+
+        self.family.set_marriage_date("05 Jan 2020")
+        self.individual.set_death_date("21 Mar 1960")
+        self.assertFalse(us05_marriage_before_death(self.family.marriage_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+
+    def test_us06_divorce_before_death(self):
+        self.assertTrue(us06_divorce_before_death(self.family.divorce_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+
+        self.family.set_divorce_date("11 Jan 2021")
+        self.individual.set_death_date("15 Jun 2007")
+        self.assertFalse(us06_divorce_before_death(self.family.divorce_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+
+        self.family.set_divorce_date("11 Mar 2000")
+        self.individual.set_death_date("15 Jun 2019")
+        self.assertTrue(us06_divorce_before_death(self.family.divorce_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+
+        self.family.set_divorce_date("11 Mar 2012")
+        self.individual.set_death_date("15 Jun 2001")
+        self.assertFalse(us06_divorce_before_death(self.family.divorce_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+
+    def test_us03_birth_before_death(self):
+        self.assertTrue(us03_birth_before_death(self.individual.birth_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id))
+
+        self.individual.set_birth_date("05 Jan 2000")
+        self.individual.set_death_date("21 Mar 1760")
+        self.assertFalse(us03_birth_before_death(self.individual.birth_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id))
+
+        self.individual.set_birth_date("05 Jan 1997")
+        self.individual.set_death_date("21 Mar 2050")
+        self.assertTrue(us03_birth_before_death(self.individual.birth_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id))
+
+        self.individual.set_birth_date("05 Jan 2056")
+        self.individual.set_death_date("21 Mar 1960")
+        self.assertFalse(us03_birth_before_death(self.individual.birth_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id))
+
+    def test_us04_marriage_before_divorce(self):
+        self.assertTrue(us04_marriage_before_divorce(self.family.marriage_date, self.family.divorce_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+
+        self.individual.set_birth_date("14 Jun 1997")
+        self.family.set_marriage_date("11 Mar 2021")
+        self.family.set_divorce_date("15 Jun 2010")
+        self.assertFalse(us04_marriage_before_divorce(self.family.marriage_date, self.family.divorce_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+
+        self.individual.set_birth_date("17 Oct 1960")
+        self.family.set_marriage_date("11 Mar 2004")
+        self.family.set_divorce_date("15 Jun 2010")
+        self.assertTrue(us04_marriage_before_divorce(self.family.marriage_date, self.family.divorce_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+
+        self.individual.set_birth_date("11 Jun 1964")
+        self.family.set_marriage_date("11 Mar 2016")
+        self.family.set_divorce_date("15 Jun 2010")
+        self.assertFalse(us04_marriage_before_divorce(self.family.marriage_date, self.family.divorce_date, self.individual.get_full_name(), self.individual.id, self.family.id))
 
     def test_us07_age_less_than_150(self):
-        self.assertTrue(us07_age_less_than_150(self.individual.birth_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+        self.assertTrue(us07_age_less_than_150(self.individual.birth_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id))
         
         self.individual.set_birth_date("15 Feb 2012")
         self.individual.set_death_date("21 Jan 2000")
         
-        self.assertFalse(us07_age_less_than_150(self.individual.birth_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+        self.assertFalse(us07_age_less_than_150(self.individual.birth_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id))
         
         self.individual.set_birth_date("05 Jan 1960")
         self.individual.set_death_date("21 Mar 2000")
         
-        self.assertTrue(us07_age_less_than_150(self.individual.birth_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+        self.assertTrue(us07_age_less_than_150(self.individual.birth_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id))
         
         self.individual.set_birth_date("25 SEP 1760")
         self.individual.set_death_date("11 Jan 2000")
         
-        self.assertFalse(us07_age_less_than_150(self.individual.birth_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id, self.family.id))
+        self.assertFalse(us07_age_less_than_150(self.individual.birth_date, self.individual.death_date, self.individual.get_full_name(), self.individual.id))
     
     def test_us08_birth_before_marriage_of_parents(self):
         self.individual.set_birth_date("15 Feb 2008")
@@ -77,7 +141,108 @@ class TestValidateDataMethod(unittest.TestCase):
         self.family.set_marriage_date("01 Sep 2004")
         self.family.set_divorce_date("15 Jun 2010")
         
-        self.assertFalse(us08_birth_before_marriage_of_parents(self.individual.birth_date, self.family.marriage_date, self.family.divorce_date, self.individual.get_full_name(), self.individual.id, self.family.id))
-    
+        self.assertFalse(us08_birth_before_marriage_of_parents(self.individual.birth_date, self.family.marriage_date, self.family.divorce_date, self.individual.get_full_name(), self.individual.id, self.family.id))   
+        
+    def test_us15_fewer_than_15_siblings(self):
+        self.family.set_husb("I0")
+        self.family.set_wife("I1")
+        self.family.set_children("I2")
+        self.family.set_children("I3")
+        self.family.set_children("I4")
+        self.family.set_children("I5")
+        self.family.set_children("I6")
+        self.family.set_children("I7")
+        self.family.set_children("I8")
+        self.family.set_children("I9")
+        self.family.set_children("I10")
+        self.family.set_children("I11")
+        self.family.set_children("I12")
+        self.family.set_children("I13")
+        self.family.set_children("I14")
+        self.family.set_children("I15")
+        self.family.set_children("I16")
+        self.family.set_children("I17")
+        self.assertFalse(us15_fewer_than_15_siblings(self.family))
+        
+        self.family.children = []
+        self.family.set_children("I2")
+        self.family.set_children("I3")
+        self.family.set_children("I4")
+        self.family.set_children("I5")
+        self.family.set_children("I6")
+        self.family.set_children("I7")
+        self.family.set_children("I8")
+        self.assertTrue(us15_fewer_than_15_siblings(self.family))
+        
+    def test_us16_male_last_names(self):
+        individuals = []
+        self.individual = Individual("Indv0")
+        self.individual.set_name("Pablo /Escobar/")
+        self.individual.set_gender('M')
+        self.family.set_husb("Indv0")
+        individuals.append(self.individual)
+        self.individual = Individual("Indv1")
+        self.individual.set_name("Veronika /Esco/")
+        self.individual.set_gender('F')
+        self.family.set_wife("Indv1")
+        individuals.append(self.individual)
+        self.individual = Individual("Indv2")
+        self.individual.set_name("Terry /Escobart/")
+        self.individual.set_gender('M')
+        self.family.set_children("Indv2")
+        individuals.append(self.individual)
+        self.individual = Individual("Indv3")
+        self.individual.set_name("Maria /Escobar/")
+        self.individual.set_gender('F')
+        self.family.set_children("Indv3")
+        individuals.append(self.individual)
+        self.assertFalse(us16_male_last_names(self.family.husb, self.family.wife, self.family.children, individuals))
+        
+        individuals = []
+        self.individual = Individual("Indv0")
+        self.individual.set_name("Danis /Hazard/")
+        self.individual.set_gender('M')
+        self.family.set_husb("Indv0")
+        individuals.append(self.individual)
+        self.individual = Individual("Indv1")
+        self.individual.set_name("Vina /Hazard/")
+        self.individual.set_gender('F')
+        self.family.set_wife("Indv1")
+        individuals.append(self.individual)
+        self.individual = Individual("Indv2")
+        self.individual.set_name("JR /Hazard/")
+        self.individual.set_gender('M')
+        self.family.set_children("Indv2")
+        individuals.append(self.individual)
+        self.individual = Individual("Indv3")
+        self.individual.set_name("SR /Hazard/")
+        self.individual.set_gender('M')
+        self.family.set_children("Indv3")
+        individuals.append(self.individual)
+        self.assertTrue(us16_male_last_names(self.family.husb, self.family.wife, self.family.children, individuals))
+
+        individuals = []
+        self.individual = Individual("Indv0")
+        self.individual.set_name("Nick /Walter/")
+        self.individual.set_gender('M')
+        self.family.set_husb("Indv0")
+        individuals.append(self.individual)
+        self.individual = Individual("Indv1")
+        self.individual.set_name("Moni /Walter/")
+        self.individual.set_gender('F')
+        self.family.set_wife("Indv1")
+        individuals.append(self.individual)
+        self.individual = Individual("Indv2")
+        self.individual.set_name("Maris /Walter/")
+        self.individual.set_gender('M')
+        self.family.set_children("Indv2")
+        individuals.append(self.individual)
+        self.individual = Individual("Indv3")
+        self.individual.set_name("Haly /Walters/")
+        self.individual.set_gender('M')
+        self.family.set_children("Indv3")
+        individuals.append(self.individual)
+        self.assertFalse(us16_male_last_names(self.family.husb, self.family.wife, self.family.children, individuals))
+
 if __name__ == "__main__":
     unittest.main(exit=False)
--- ValidateData.py
+++ ValidateData.py
@@ -34,21 +34,55 @@ def us02_birth_before_marriage(birth_date, marriage_date, indiv_name, indiv_id, 
     else:
         return False
 
-def us07_age_less_than_150(birth_date, death_date, indiv_name, indiv_id, family_id) -> bool:
+
+def us03_birth_before_death(birth_date, death_date, indiv_name, indiv_id):
+    if birth_date > death_date:
+        ReportUtils.add_error_found("Error US03: Death date of " + str(indiv_name) + " (" + str(indiv_id) + ") occurs before birth date.")
+        return False
+    else:
+        return True
+
+
+def us04_marriage_before_divorce(marriage_date, divorce_date, indiv_name, indiv_id, family_id):
+    if marriage_date > divorce_date:
+        ReportUtils.add_error_found("Error US04: Divorce date of " + str(indiv_name) + " (" + str(indiv_id) + ") occurs before marriage date. (" + str(family_id) + ")")
+        return False
+    else:
+        return True
+
+
+def us05_marriage_before_death(marriage_date, death_date, indiv_name, indiv_id, family_id):
+    if marriage_date > death_date:
+        ReportUtils.add_error_found("Error US05: Marriage date of " + str(indiv_name) + " (" + str(indiv_id) + ") occurs before death date. (" + str(family_id) + ")")
+        return False
+    else:
+        return True
+
+
+def us06_divorce_before_death(divorce_date, death_date, indiv_name, indiv_id, family_id):
+    if divorce_date > death_date:
+        ReportUtils.add_error_found("Error US06: Divorce date of " + str(indiv_name) + " (" + str(indiv_id) + ") occurs before death date. (" + str(family_id) + ")")
+        return False
+    else:
+        return True
+
+
+def us07_age_less_than_150(birth_date, death_date, indiv_name, indiv_id) -> bool:
     current_date: datetime = datetime.now()
     years_150 = timedelta(days=54750)
 
     if death_date:
         if birth_date - death_date > timedelta(days=0): 
-            ReportUtils.add_error_found("Error US07: Age of " + str(indiv_name) + " (" + str(indiv_id) + ") is Greater than 150 years. (" + str(family_id) + ")")
+            ReportUtils.add_error_found("Error US07: Age of " + str(indiv_name) + " (" + str(indiv_id) + ") is Greater than 150 years.")
             return False
         elif death_date - birth_date < years_150: 
             return True
     else:
         if current_date - birth_date < years_150: return True
-    ReportUtils.add_error_found("Error US07: Age of " + str(indiv_name) + " (" + str(indiv_id) + ") is not Greater than 150 years. (" + str(family_id) + ")")
+    ReportUtils.add_error_found("Error US07: Age of " + str(indiv_name) + " (" + str(indiv_id) + ") is not Greater than 150 years.")
             
     return False
+
 
 def us08_birth_before_marriage_of_parents(birth_date, marriage_date, divorce_date, indiv_name, indiv_id, family_id) -> bool:
     
@@ -56,12 +90,29 @@ def us08_birth_before_marriage_of_parents(birth_date, marriage_date, divorce_dat
         if birth_date - marriage_date > timedelta(minutes=0) and birth_date - divorce_date < timedelta(days=275):
             return True
         else:
-            ReportUtils.add_error_found("Error US08:  "+ str(indiv_name) +  " (" + str(indiv_id) + ") has not birth before the marrige of Parents. (" + str(family_id) + ")")
+            ReportUtils.add_error_found("Error US08:  "+ str(indiv_name) +  " (" + str(indiv_id) + ") has not birth before the marriage of Parents. (" + str(family_id) + ")")
             return False
     else:
-        ReportUtils.add_error_found("Error US08:  "+ str(indiv_name) +  " (" + str(indiv_id) + ") Parents did not done Marrige. (" + str(family_id) + ")")
+        ReportUtils.add_error_found("Error US08:  "+ str(indiv_name) +  " (" + str(indiv_id) + ") Parents did not done Marriage. (" + str(family_id) + ")")
+
+def us15_fewer_than_15_siblings(family) -> bool:
+    if len(family.children) < 15:
+        print("Success: Family (" + family.id +  ") : Siblings are less than 15")
+        return True
+    else:
+        print("Error US15: Family (" + family.id + ") : Siblings are greater than 15")
         return False
 
+def us16_male_last_names(husb, wife, child, individuals):
+    ids = [husb, wife]
+    ids.extend(child)
+    males = [individual for individual in individuals if individual.gender == 'M' and individual.id in ids]
+    names = [male.name.split('/')[1] for male in males]
+    if len(set(names)) == 1:
+        return True
+    else:
+        print("Error US16: This Male has differnet last name or has no last Name.")
+        return False
 
 def is_indiv_valid(indiv):
     is_valid = True
@@ -103,7 +154,8 @@ def validate_data(individuals, families):
 
             if indiv.death_date is not None:
                 us01_dates_before_current_date(indiv.death_date, 'Death date', indiv)
-                us07_age_less_than_150(indiv.birth_date, indiv.death_date, indiv.get_full_name, str(indiv.id))
+                us03_birth_before_death(indiv.birth_date, indiv.death_date, indiv.get_full_name(), str(indiv.id))
+                us07_age_less_than_150(indiv.birth_date, indiv.death_date, indiv.get_full_name(), str(indiv.id))
 
             if indiv.get_family_id_as_spouse() != 'NA':
                 for family_id in indiv.family_id_as_spouse:
@@ -111,6 +163,18 @@ def validate_data(individuals, families):
 
                     if family_data is not None and family_data.marriage_date is not None:
                         us02_birth_before_marriage(indiv.birth_date, family_data.marriage_date, indiv.get_full_name(), str(indiv.id), str(family_id))
+
+                        if family_data.divorce_date is not None:
+                            us08_birth_before_marriage_of_parents(indiv.birth_date, family_data.marriage_date,
+                                                                  family_data.divorce_date, indiv.get_full_name(),
+                                                                  str(indiv.id), str(family_id))
+                            us04_marriage_before_divorce(family_data.marriage_date, family_data.divorce_date, indiv.get_full_name(), str(indiv.id), str(family_id))
+                            if indiv.death_date is not None:
+                                us06_divorce_before_death(family_data.divorce_date, indiv.death_date,
+                                                          indiv.get_full_name(), str(indiv.id), str(family_id))
+
+                        if indiv.death_date is not None:
+                            us05_marriage_before_death(family_data.marriage_date, indiv.death_date, indiv.get_full_name(), str(indiv.id), str(family_id))
 
     for family in family_list:
         if is_fam_valid(family):
@@ -122,8 +186,5 @@ def validate_data(individuals, families):
             if indiv.get_family_id_as_spouse() != 'NA':
                 for family_id in indiv.family_id_as_spouse:
                     family_data = get_family_by_family_id(family_id)
-
-                    if family_data is not None and family_data.marriage_date is not None:
-                        us08_birth_before_marriage_of_parents(indiv.birth_date, family_data.marriage_date,  family_data.divorce_date, indiv.get_full_name(), str(indiv.id), str(family_id))
 
     ReportUtils.compile_report()
